06634a06ca30800dfaace126e7908d4f52b75abf
BeruboIV <hardikraj13@gmail.com>
2021-06-27 18:56:54 +0530

    Removed price, location from shcema and added genre

--- models/anime.js
+++ models/anime.js
@@ -3,10 +3,15 @@ const Schema = mongoose.Schema; // Shortens monoose.Schema to Schema
 
 const AnimeSchema = new Schema({
     title: String,
-    character: String,
+    genre: String,
     image: String,
     description: String,
-    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review",
+        },
+    ],
 });
 
 module.exports = mongoose.model("Anime", AnimeSchema);
